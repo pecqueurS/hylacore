c91c4c3a64b73a30eb9537fa3197c9f8696cfbbf
Oscar <stephane.pecqueur@gmail.com>
2015-07-21 02:31:46 +0200

    add autoloader classes PSR4

---
+++ src/core/Autoloader/autoloader.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__.'/ClassLoader/Psr4ClassLoader.php';
+
+use Symfony\Component\ClassLoader\Psr4ClassLoader as ClassLoader;
+use Hyla\Config\Conf;
+
+$loader = new ClassLoader();
+$loader->addPrefix('Hyla',dirname(__DIR__));
+
+$loader->register();
+
+Conf::initApp();
+$app = Conf::get('app');
+if (!empty($app)) {
+    $loader->addPrefix($app['name'], Conf::$rootDir . $app['path']);
+}
+
+// Load ErrorHandler Class
+//require_once dirname(__DIR__).'/ErrorHandling/ErrorHandling.class.php';
