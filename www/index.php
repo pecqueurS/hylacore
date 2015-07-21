3d690c7a8b34423320dbb904dea6f2bdbb27d38c
Oscar <stephane.pecqueur@gmail.com>
2015-07-21 02:35:50 +0200

    add index file and config

--- www/index.php
+++ www/index.php
@@ -1,10 +1,17 @@
 <?php
+use Hyla\Config\Conf;
+use Hyla\FrontController\FrontController;
 
-echo 'hello world !'
+require_once ("../src/core/Autoloader/autoloader.php");
+
+Conf::init();
+FrontController::init();
+
+var_dump($_SERVER);
 
 
 
-
-
+var_dump(Conf::getAll());
+echo 'hello world !';
 
 ?>
