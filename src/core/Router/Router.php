d5928c8fa039f51964d6aaeb01be9c39174a0cf1
Stephane <stephane.pecqueur@gmail.com>
2015-08-19 02:53:20 +0200

    add route informations retriever

--- src/core/Router/Router.php
+++ src/core/Router/Router.php
@@ -2,6 +2,7 @@
 
 namespace Hyla\Router;
 use Hyla\Config\Conf;
+use Hyla\Server\Server;
 
 /**
  * Class Router
@@ -9,17 +10,46 @@ use Hyla\Config\Conf;
  */
 abstract class Router {
 
+    /**
+     * Path to routing for app
+     */
     const ROUTING_PATH = 'etc/routing/';
 
+    /**
+     * @var array application conf
+     */
     private static $appConf;
 
+    /**
+     * @var array all routes for an app
+     */
+    private static $allRoutes = array();
+
+    /**
+     * @var array route informations
+     */
+    private static $routeInfos = array();
+
+
+    /**
+     * Init routing for app
+     *
+     * @throws \Exception
+     */
     public static function init()
     {
         self::loadRoutes();
-        self::matchRoute();
+        self::createRouteInfos(self::matchRoute());
+
+        self::addGetParameters();
+
+        Conf::set('routeInfo', self::$routeInfos);
     }
 
 
+    /**
+     * Load routes from app folder
+     */
     private static function loadRoutes()
     {
         self::$appConf = Conf::get('app');
@@ -32,13 +62,17 @@ abstract class Router {
                     Conf::add($file, 'route', true);
                 }
             }
-
         } else {
             Conf::set('route', []);
         }
     }
 
 
+    /**
+     * Load files routing from app folder
+     *
+     * @return array
+     */
     private static function loadFiles()
     {
         $path = self::$appConf['path'] . '/' . self::ROUTING_PATH;
@@ -55,25 +89,28 @@ abstract class Router {
     }
 
 
+    /**
+     * Retrieve route used
+     *
+     * @return array
+     */
     private static function matchRoute()
     {
         $routing = Conf::get('route');
+        $routeUri = self::getRouteUri();
 
-        var_dump($routing);
-        $test = microtime();
         $routes = array();
-        $routingFormatted = array();
         foreach ($routing as $routeName => $routeTest) {
-            $routeNameFormatted = substr($routeName, 0,32);
+            $routeNameFormatted = substr($routeName, 0, 32);
             $routes[] = '^(?<' . $routeNameFormatted . '>' . $routeTest['pattern'] . ')$';
-            $routingFormatted[$routeNameFormatted] = $routeTest;
+            self::$allRoutes[$routeNameFormatted] = $routeTest;
         }
-        $testPattern = '#'.implode('|', $routes).'#';
-        var_dump($testPattern);
+        $testPattern = '#' . implode('|', $routes) . '#';
+
         preg_match($testPattern, $routeUri, $argv);
-        unset($argv[0]);
+        array_shift($argv);
         $isFound = false;
-        $results = array_filter($argv, function($value, $key) use (&$isFound) {
+        $results = array_filter($argv, function ($value, $key) use (&$isFound) {
             $resultFilter = is_string($key) && $value != '';
             if (!$isFound && $resultFilter) {
                 $isFound = true;
@@ -83,26 +120,95 @@ abstract class Router {
 
             return $isFound ? true : $resultFilter;
         }, ARRAY_FILTER_USE_BOTH);
+
+        return $results;
+    }
+
+
+    /**
+     * Create route informations
+     *
+     * @param $routeParameters
+     * @throws \Exception
+     */
+    private static function createRouteInfos($routeParameters)
+    {
         $setName = true;
         $pattern = '';
-        foreach ($results as $routeNameFormatted => $result) {
+        foreach ($routeParameters as $routeNameFormatted => $result) {
             if ($setName === true) {
                 $pattern = $result;
-                if (!empty($routingFormatted[$routeNameFormatted])) {
-                    $routeInfos = $routingFormatted[$routeNameFormatted];
-                    Logger::log('['.__CLASS__.'] route matches -> '.$routeNameFormatted, Logger::LOG_DEBUG);
-                    $routeInfos['argv'] = array();//$this->getParams;
-                    $routeInfos['name'] = $this->getRouteAppLoaded().':'.$routeNameFormatted;
-                    Logger::log('['.__CLASS__.'] route loaded -> '.$this->getRouteAppLoaded().':'.$routeNameFormatted, Logger::LOG_DEBUG);
+                if (!empty(self::$allRoutes[$routeNameFormatted])) {
+                    self::$routeInfos = self::$allRoutes[$routeNameFormatted];
+                    //Logger::log('['.__CLASS__.'] route matches -> '.$routeNameFormatted, Logger::LOG_DEBUG);
                 }
-
                 $setName = false;
-            }elseif ($result !== $pattern) {
-                $routeInfos['argv'][] = $result;
+            } elseif ($result !== $pattern) {
+                self::$routeInfos['argv'][] = $result;
             }
         }
-        var_dump($routeInfos);
-        $test2 = microtime();
-        var_dump($test2 - $test);
+
+        if (empty(self::$routeInfos)) {
+            throw new \Exception('route no match !');
+        }
+    }
+
+
+    /**
+     * Add $_GET parameters to route informations
+     *
+     * @throws \Exception
+     */
+    private static function addGetParameters()
+    {
+        if (!empty(self::$routeInfos['parameters'])) {
+            $getParameters = self::getUriParameters();
+            foreach(self::$routeInfos['parameters'] as $key => $pattern) {
+                if (preg_match('#^'. $pattern .'$#', (!empty($getParameters[$key]) ? $getParameters[$key] : ''), $argv)) {
+                    $parameter = array_shift($argv);
+                    self::$routeInfos['argv'][] = $parameter !== '' ? $parameter : null;
+                } else {
+                    throw new \Exception('invalid parameters !');
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Return URI
+     *
+     * @return null|string
+     */
+    private static function getRouteUri()
+    {
+        if (Conf::$isCli) {
+            $argvServer = Server::get('argv');
+            array_shift($argvServer);
+            return implode($argvServer, ' ');
+        } else {
+            $requestUriServer = Server::get('requestUri');
+            $explodedRoute = explode('?', $requestUriServer);
+            return (!empty($explodedRoute[0])) ? $explodedRoute[0] : $requestUriServer;
+        }
+    }
+
+
+    /**
+     * Return $_GET parameters from URI informations Server
+     *
+     * @return array
+     */
+    private static function getUriParameters()
+    {
+        $queryStringServer = Server::get('queryString');
+        $queryStringExploded = explode('&', $queryStringServer);
+        $getParameters = array();
+        foreach ($queryStringExploded as $parameter) {
+            list($key, $value) = explode('=', $parameter);
+            $getParameters[$key] = $value;
+        }
+
+        return $getParameters;
     }
 }
